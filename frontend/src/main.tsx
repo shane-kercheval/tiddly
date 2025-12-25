c2f4423189e63ccc55c4aede665439edeead5867
Shane Kercheval <shane-kercheval@users.noreply.github.com>
2025-12-25 14:53:59 -0800

    fix ignored file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { queryClient } from './lib/queryClient'
+import { queryClient } from './queryClient'
 import './index.css'
 import App from './App.tsx'
 
